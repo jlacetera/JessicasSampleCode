9942ce84b0445d1a39ebba03b80f6d672b84d7d8
jlacetera <jlacetera@aol.com>
2014-09-08 22:05:12 -0400

    Revert "Initial commit of sample files"
    This reverts commit fda23a96989bcf1089b733868a8e689a62308646.

--- tableSorterScriptsCSS.php
+++
@@ -1,8 +0,0 @@
-
-<!-- table Sorter files -->
- 
-<link rel="stylesheet" href="Styles/TableStyle.css">
-<script src="javascript/jquery.tablesorter.js"></script> <!-- tablesorter plugin -->
-<script src="javascript/jquery.tablesorter.widgets.js"></script> <!-- tablesorter widget file - loaded after the plugin -->
-<script src='javascript/viewTableSorterScripts.js' type='text/javascript'></script>
-
